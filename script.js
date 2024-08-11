adac8b6cf79b9ade9254d45e4ad95bd1570db557
Gowtham K <gowthamk3543@gmail.com>
2024-08-11 17:25:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const downloadCvButton = document.getElementById('download-cv');
 
 downloadCvButton.addEventListener('click', () => {
   const cvLink = document.createElement('a');
-  cvLink.href = './cv.pdf'; // replace with your CV file path
-  cvLink.download = 'YourName_Resume-AWS&DevOps.pdf'; // replace with your desired file name
+  cvLink.href = './Resume-AWS&DevOps.pdf'; // replace with your CV file path
+  cvLink.download = 'Resume-AWS&DevOps.pdf'; // replace with your desired file name
   cvLink.click();
 });
